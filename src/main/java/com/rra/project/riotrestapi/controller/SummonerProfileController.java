e797f74a9ee7aef7bc01ccde6a35755d3d9cbc3f
tgxyh <tomaszgutkowski8@gmail.com>
2026-07-26 01:54:41 +0200

    port and cors moved to env variables added dockerfile

--- src/main/java/com/rra/project/riotrestapi/controller/SummonerProfileController.java
+++ src/main/java/com/rra/project/riotrestapi/controller/SummonerProfileController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "${cors.allowed-origin}")
 public class SummonerProfileController {
 
     private final RiotApiService riotApiService;
